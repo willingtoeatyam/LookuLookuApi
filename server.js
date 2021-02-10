dc1ec251d6e537a053323076dc843d3ebc7d299e
William Imoukhuede <wimoukhuede@gmail.com>
2021-02-10 11:40:36 +0100

    heroku test

--- server.js
+++ server.js
@@ -27,8 +27,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
  
-app.get('/', (req, res) => {
-    res.send(database.users);
+app.get('/', (req, res) => {res.send('it works');
 })
 
 app.post('/signin', (req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
