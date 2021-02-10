2b93049dff51982bd93ff7abc18dc98c49e0a286
William Imoukhuede <wimoukhuede@gmail.com>
2021-02-10 12:20:48 +0100

    heroku db error

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'host',
+      host : process.env.host,
       user : 'postgres',
       password : ' ',
       database : 'lookulooku'
